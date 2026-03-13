d70e5c63fc00e91e1136d32229d9476e05a0df0c
bahyfamily <bahyfamily@gmail.com>
2026-03-13 11:29:36 +0000

    Bug Fixes

--- showcase-site/vite.config.ts
+++ showcase-site/vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.html'], // tell vite to treat html in public/src as assets if needed
   base: '/Modern-Navbar-Collection/', // relative paths for github pages
 })
